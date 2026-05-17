2f90fb2125103ddc2d4e4dbf2d707979e0e3f74a
Anshuman <anshumanpachauri26@gmail.com>
2026-05-17 12:00:00 +0530

    Completed implementation of apiGateway.

---
+++ src/main/java/com/ap/microservices/gateway/routes/Routes.java
@@ -0,0 +1,53 @@
+package com.ap.microservices.gateway.routes;
+
+import org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class Routes {
+
+    @Bean
+    public RouterFunction<ServerResponse> productServiceRouter() {
+
+        return GatewayRouterFunctions.route("product_service")
+                .route(
+                    // Changed from "/api/product/" to match the base path and all sub-paths
+                    RequestPredicates.path("/api/product/**"), 
+                    HandlerFunctions.http()
+                )
+                .before(BeforeFilterFunctions.uri("http://localhost:8080"))
+                .build();
+    }
+    
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceRouter() {
+
+        return GatewayRouterFunctions.route("order_service")
+                .route(
+                    // Changed from "/api/product/" to match the base path and all sub-paths
+                    RequestPredicates.path("/api/order/**"), 
+                    HandlerFunctions.http()
+                )
+                .before(BeforeFilterFunctions.uri("http://localhost:8081"))
+                .build();
+    }
+    
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceRouter() {
+
+        return GatewayRouterFunctions.route("inventory_service")
+                .route(
+                    // Changed from "/api/product/" to match the base path and all sub-paths
+                    RequestPredicates.path("/api/inventory/**"), 
+                    HandlerFunctions.http()
+                )
+                .before(BeforeFilterFunctions.uri("http://localhost:8082"))
+                .build();
+    }
+}
